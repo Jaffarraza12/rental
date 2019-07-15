5faedd91e6bc107ccafd04d22455a8020024ff06
Jaffar <jaffaraza@gmail.com>
2019-07-15 15:49:30 +0500

    update envirnoment testing

--- app/Http/Model/PaymentReceived.php
+++ app/Http/Model/PaymentReceived.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PaymentReceived extends Model
 {
-    protected $table = 'payment_Debit';
+    protected $table = 'payment_debit';
     public $timestamps = true;
     protected $primaryKey = "id";
 }
